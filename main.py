9867c4a95c20c6972b443202372723b4d73b7011
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-21 05:15:39 -0700

    smart_contact

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import tkinter as tk
 from tkinter import messagebox
 import csv
 import os
+
+
+
+# Chemin du fichier CSV
+FICHIER = "contacts.csv"
+
+# Fonction pour enregistrer un contact
+def enregistrer_contact():
+    nom = name_var.get()
+    telephone = phone_var.get()
+    email = email_var.get()
+    genre = gender_var.get()
